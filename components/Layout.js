ab08d5f328641fa5a7e3425eda9ad773dbc4c3df
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-05-17 18:30:12 +0530

    Footer

--- components/Layout.js
+++ components/Layout.js
@@ -1,29 +1,25 @@
 import Head from 'next/head';
 import styles from '../styles/Layout.module.css';
+import Footer from './Footer';
 import Header from './Header';
-export default function Layout({title,descriptiton,keyword,children}) {
-    return (
-        <div>
-            <Head>
-            <title>{title}</title>
-            <meta name='description' content={descriptiton}  />
-            <meta name='keyword' content={keyword} />
+export default function Layout({ title, descriptiton, keyword, children }) {
+	return (
+		<div>
+			<Head>
+				<title>{title}</title>
+				<meta name='description' content={descriptiton} />
+				<meta name='keyword' content={keyword} />
+			</Head>
 
-            </Head>
+			<Header />
 
-            <Header/>
-
-            <div className={styles.container}>
-            {children}
-            </div>
-          
-        </div>
-    )
+			<div className={styles.container}>{children}</div>
+			<Footer />
+		</div>
+	);
 }
 
-
-Layout.defaultProps ={
-    title:'DJ Events || Hottest parties in town',
-    descriptiton:'Best DJ events in the town,catch up soon',
-
-}
+Layout.defaultProps = {
+	title: 'DJ Events || Hottest parties in town',
+	descriptiton: 'Best DJ events in the town,catch up soon',
+};
