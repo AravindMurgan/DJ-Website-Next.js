4f47675b73f899143036a43ce88da7abee749991
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-05-18 10:30:28 +0530

    eventsAPI

--- pages/api/events/index.js
+++ pages/api/events/index.js
@@ -1,6 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-const {events} = require('./data.json')
+const { events } = require('./data.json');
 
 export default (req, res) => {
-  res.status(200).json(events)
-}
+	res.status(200).json(events);
+
+	if (req.method === 'GET') {
+		res.status(200).json(events);
+	} else {
+		res.setHeader('Allow', ['GET']);
+		res.status(405).json({
+			messgage: `Method ${req.method}
+    is not allowed `,
+		});
+	}
+};
