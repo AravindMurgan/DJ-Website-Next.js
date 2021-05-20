e96dbd1ba86c6f5f7dea73318b86e4131710859e
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-05-20 08:23:08 +0530

    slug-event-page

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,28 +1,47 @@
 import Layout from '@/components/Layout';
 import { API_URL } from '@/config/index';
+import styles from '@/styles/Event.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function EventPage({ evt }) {
-	console.log(evt);
-	return ( 
+	return (
 		<Layout>
-			<h3>{evt.name}</h3>
-			<p>test</p>
+			<div className={styles.event}>
+				<h1>{evt.name} </h1>
+				{evt.image && (
+					<div className={styles.image}>
+						<Image src={evt.image} width={960} height={600} />
+					</div>
+				)}
+
+				<h3>Performers: </h3>
+				<p>{evt.performers}</p>
+				<h3>Description:</h3>
+				<p>{evt.description}</p>
+				<h3>Venue: {evt.venue} </h3>
+				<p>{evt.address} </p>
+
+				<Link href='/events'>
+					<a className={styles.back}>{'<'} Go Back </a>
+				</Link>
+			</div>
 		</Layout>
 	);
 }
 
-export async function getStaticPaths(){
-    const res = await fetch(`${API_URL}/api/events`)
-    const events = await res.json()
+export async function getStaticPaths() {
+	const res = await fetch(`${API_URL}/api/events`);
+	const events = await res.json();
 
-    const paths = events.map(evt =>({
-        params: {slug: evt.slug}
-    }) )
+	const paths = events.map((evt) => ({
+		params: { slug: evt.slug },
+	}));
 
-    return {
-        paths,
-        fallback: true
-    } 
+	return {
+		paths,
+		fallback: true,
+	};
 }
 
 export async function getStaticProps({ params: { slug } }) {
@@ -31,7 +50,7 @@ export async function getStaticProps({ params: { slug } }) {
 
 	return {
 		props: { evt: events[0] },
-        revalidate:1
+		revalidate: 1,
 	};
 }
 
