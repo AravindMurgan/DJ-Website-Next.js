e887f98293922f23768e0c9db03512c31b2400b3
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-05-21 19:18:02 +0530

    strapi and next.js config

--- components/EventsItem.js
+++ components/EventsItem.js
@@ -7,25 +7,28 @@ export default function EventsItem({ evt }) {
 		<div className={styles.event}>
 			<div className={styles.img}>
 				<Image
-					src={evt.image ? evt.image : '/images/event-default.png'}
+					src={
+						evt.image
+							? evt.image.formats.thumbnail.url
+							: '/images/event-default.png'
+					}
 					width={170}
 					height={100}
 				/>
 			</div>
 
-            <div className={styles.info} >
-                <span>
-                    {evt.date} at {evt.time}
-                </span>
-                <h3>{evt.name} </h3>
+			<div className={styles.info}>
+				<span>
+					{evt.date} at {evt.time}
+				</span>
+				<h3>{evt.name} </h3>
+			</div>
 
-            </div>
-
-            <div className={styles.link} >
-                <Link href={`/events/${evt.slug}`} >
-                    <a className='btn' >Details</a>
-                </Link>
-            </div>
+			<div className={styles.link}>
+				<Link href={`/events/${evt.slug}`}>
+					<a className='btn'>Details</a>
+				</Link>
+			</div>
 		</div>
 	);
 }
