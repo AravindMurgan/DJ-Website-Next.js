97aeadba23897a29c62afd3e8d61edf43f9a0a3f
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-05-27 11:34:08 +0530

    prev&next-pages

---
+++ components/Pagination.js
@@ -0,0 +1,27 @@
+import { PER_PAGE } from '@/config/index';
+import Link from 'next/link';
+
+export default function Pagination({ events, page, total }) {
+    const lastpage = Math.ceil(total / PER_PAGE);
+    return (
+        <div>
+            <h1> Events</h1>
+			{events.length === 0 && <h3>No events to show</h3>}
+			{events.map((evt) => (
+				<EventsItem key={evt.id} evt={evt} />
+			))}
+
+			{page > 1 && (
+				<Link href={`/events?page=${page - 1}`}>
+					<a className='btn-secondary'>Prev</a>
+				</Link>
+			)}
+
+			{page < lastpage && (
+				<Link href={`/events?page=${page + 1}`}>
+					<a className='btn-secondary'>Next</a>
+				</Link>
+			)}
+        </div>
+    )
+}
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,37 +1,22 @@
-import EventsItem from '@/components/EventsItem';
 import Layout from '@/components/Layout';
+import Pagination from '@/components/Pagination';
 import { API_URL, PER_PAGE } from '@/config/index';
-import { useRouter } from 'next/router';
 
-export default function EventsPage({ events,page,total }) {
-	const lastpage = Math.ceil(totla/PER_PAGE)
-	const router = useRouter();
+export default function EventsPage({ events, page, total }) {
 	return (
 		<Layout>
-			<h1> Events</h1>
-			{events.length === 0 && <h3>No events to show</h3>}
-			{events.map((evt) => (
-				<EventsItem key={evt.id} evt={evt} />
-			))}
-
-			{page > 1 && (
-				<Link href={`/events?page=${page -1}`} >
-					<a className='btn-secondary' >Prev</a>
-				</Link>
-			) }
-
+			<Pagination events={events} page={page} total={total} />
 		</Layout>
 	);
 }
 
 export async function getServerSideProps({ query: { page = 1 } }) {
-	
 	//calculate start page//
-	const start = +page === 1 ? 0 : (+page-1)* PER_PAGE
-	
+	const start = +page === 1 ? 0 : (+page - 1) * PER_PAGE;
+
 	//total Events//
-	const totalRes = await fetch(`${API_URL}/events/count`)
-	const total = totalRes.json()
+	const totalRes = await fetch(`${API_URL}/events/count`);
+	const total = await totalRes.json();
 
 	//Fetch Events
 	const eventRes = await fetch(
@@ -40,6 +25,6 @@ export async function getServerSideProps({ query: { page = 1 } }) {
 	const events = await eventRes.json();
 
 	return {
-		props: { events,page: +page ,total },
+		props: { events, page: +page, total },
 	};
 }
