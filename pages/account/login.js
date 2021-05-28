a992816c61af6402f045f187be3ffc97619db9d3
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-05-28 11:25:28 +0530

    loginComponent

--- pages/account/login.js
+++ pages/account/login.js
@@ -1,7 +1,16 @@
+import Layout from "@/components/Layout";
+import Link from "next/link";
+import { useContext, useEffect, useState } from "react";
+import { FaUser } from "react-icons/fa"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import styles from '@/styles/AuthForm.module.css'
+
+
 export default function LoginPage() {
     return (
         <div>
-            <h1>Login</h1>
+            <h1>Login </h1>
         </div>
     )
 }
