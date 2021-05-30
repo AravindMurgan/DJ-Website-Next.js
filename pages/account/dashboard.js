d11aee79d366fb1826a5d976070005585bbb50d6
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-05-30 20:06:15 +0530

    dashboard-styles and get token from server

--- pages/account/dashboard.js
+++ pages/account/dashboard.js
@@ -1,10 +1,31 @@
-import Layout from "@/components/Layout"
+import Layout from '@/components/Layout';
+import { API_URL } from '@/config/index';
+import { parseCookies } from '@/helpers/index';
 
+export default function DashboardPage({ events }) {
+	console.log(events);
+	return (
+		<Layout title='Dashboard'>
+			<h1>Dashboard Page</h1>
+		</Layout>
+	);
+}
 
-export default function DashboardPage() {
-    return (
-        <Layout title='Dashboard'>
-            <h1>Dashboard Page</h1>
-        </Layout>
-    )
+export async function getServerSideProps({ req }) {
+	const { token } = parseCookies(req);
+
+	const res = await fetch(`${API_URL}/events/me`, {
+		method: 'GET',
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	});
+
+	const events = await res.json();
+
+	return {
+		props: {
+			events,
+		},
+	};
 }
