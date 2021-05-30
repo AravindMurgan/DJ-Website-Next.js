c88644cfdcc98b1a7f7077b5a7f6742dec2e868a
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-05-30 07:19:44 +0530

    cookie-library

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,36 +1,45 @@
-import { API_URL } from '@/config/index'
+import { API_URL } from '@/config/index';
 
-export default async (req,res)=>{
-    if(req.method === 'POST' ){
-        const {identifer,password}=req.body
+export default async (req, res) => {
+	if (req.method === 'POST') {
+		const { identifer, password } = req.body;
 
-        const strapiRes = await fetch(`${API_URL}/auth/local`,{
-            method:'POST',
-            headers:{
-                'Content-Type':'application/json'
-            },
-            body: JSON.stringify({
-                identifer,
-                password
-            })
-        })
+		const strapiRes = await fetch(`${API_URL}/auth/local`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				identifer,
+				password,
+			}),
+		});
 
-        const data = await strapiRes.json()
+		const data = await strapiRes.json();
 
-        if(strapiRes.ok){
-            // @todo- setCookie//
-            res.status(200).json({user:data.user})
-        }else{
-            res.status(data.statusCode).json({
-                message:data.message[0].messages[0].message
-            })
-        }
+		if (strapiRes.ok) {
+			// @todo- setCookie//
+			res.setHeader(
+				'Set-Cookie',
+				cookie.serialize('token', data.jwt, {
+					httpOnly: true,
+					secure: process.env.NODE_ENV !== 'development',
+					maxAge: 60 * 60 * 24 * 7, // 1 week//
+					sameSite: 'strict',
+					path: '/',
+				})
+			);
 
-        
-    }else{
-        res.setHeader('Allow',['POST'])
-        res.status(405).json({
-            message: `Method ${req.method} not allowed `
-        })
-    }
-}
+			res.status(200).json({ user: data.user });
+		} else {
+			res.status(data.statusCode).json({
+				message: data.message[0].messages[0].message,
+			});
+		}
+	} else {
+		res.setHeader('Allow', ['POST']);
+		res.status(405).json({
+			message: `Method ${req.method} not allowed `,
+		});
+	}
+};
